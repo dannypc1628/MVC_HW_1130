211a34524019d4162ea4fc78a02a171d1077c6c3
dannypc1628 <h152074716@gmail.com>
2019-12-11 17:09:58 +0800

    實作AccountBook的Service層

---
+++ MVC_HW_191130/Service/AccountBookService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC_HW_191130.Models;
+
+namespace MVC_HW_191130.Service
+{
+    public class AccountBookService
+    {
+        private readonly SkillTreeHomeworkEntities db;
+
+        public AccountBookService()
+        {
+            db = new SkillTreeHomeworkEntities();
+        }
+
+        public List<AccountBook> GetAllAccountBooks()
+        {
+            return this.db.AccountBook.ToList();
+        }
+
+        public AccountBook GetSingleAccountBook(System.Guid Id)
+        {
+            return this.db.AccountBook.SingleOrDefault(d => d.Id == Id);
+        }
+
+        public void Add(AccountBook accountBook)
+        {
+            db.AccountBook.Add(accountBook);
+        }
+
+        public void Edit(AccountBook old,AccountBook newAccountBook)
+        {
+            old.Categoryyy = newAccountBook.Categoryyy;
+            old.Amounttt = newAccountBook.Amounttt;
+            old.Dateee = newAccountBook.Dateee;
+        }
+
+        public void Delete(AccountBook accountBook)
+        {
+            db.AccountBook.Remove(accountBook);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+    }
+}
