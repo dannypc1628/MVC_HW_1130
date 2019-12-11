9bc7b330ed9d88d0f34dc5f7e1f0520ccfe68cf2
dannypc1628 <h152074716@gmail.com>
2019-12-11 14:40:34 +0800

    加入將int Categoryyy轉成收入支出字串方法

---
+++ MVC_HW_191130/Service/CategoryyyConvert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_HW_191130.Service
+{
+    public class CategoryyyConvert
+    {
+        public static string ToStringName(int i)
+        {
+            if (i == 0)
+                return "收入";
+            else
+                return "支出";
+        }
+    }
+}
