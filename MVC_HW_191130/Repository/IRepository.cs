d2e3a7ac321efc719457a879858c0e49db6e97ec
dannypc1628 <h152074716@gmail.com>
2019-12-13 02:31:25 +0800

    定義IUnitOfWork、IRepository介面

---
+++ MVC_HW_191130/Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace MVC_HW_191130.Repository
+{
+    public interface IRepository<T> where T:class
+    {
+        IUnitOfWork UnitOfWork { get; set; }
+
+        IQueryable<T> All();
+
+        IQueryable<T> Query(Expression<Func<T, bool>> filter);
+
+        T GetSingle(Expression<Func<T, bool>> filter);
+
+        void Create(T entity);
+
+        void Remove(T entity);
+
+    }
+}
---
+++ MVC_HW_191130/Repository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_HW_191130.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        DbContext Context { get; set; }
+
+        void Commit();
+    }
+}
