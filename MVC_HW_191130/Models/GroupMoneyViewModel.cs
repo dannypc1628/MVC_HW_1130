a59dd08820baa987d8018c167d9ad87b7b14c9f4
dannyLiu <h152074716@gmail.com>
2019-12-19 17:47:50 +0800

    新增GroupMoneyViewModel

---
+++ MVC_HW_191130/Models/GroupMoneyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_HW_191130.Models
+{
+    public class GroupMoneyViewModel
+    {
+        public MoneyViewModel Money { get; set; }
+        public List<MoneyViewModel> ListMoney { get; set; }
+    }
+}
