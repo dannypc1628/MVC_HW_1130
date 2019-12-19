59de58c2e987c735497176c97740c9303e7719d6
dannyLiu <h152074716@gmail.com>
2019-12-19 18:50:07 +0800

    新增CategoryEnum.cs

---
+++ MVC_HW_191130/Models/CategoryEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_HW_191130.Models
+{
+    public enum CategoryEnum
+    {
+        支出,
+        收入           
+    }
+}
