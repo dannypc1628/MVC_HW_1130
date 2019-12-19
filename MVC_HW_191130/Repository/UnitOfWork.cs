11de092dcfa82073a925fb03f230cc8d92769e72
dannyLiu <h152074716@gmail.com>
2019-12-19 16:17:15 +0800

    建立Repository.cs與UnitOfWork.cs

---
+++ MVC_HW_191130/Repository/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MVC_HW_191130.Models;
+
+namespace MVC_HW_191130.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; set; }
+
+        public UnitOfWork()
+        {
+            Context = new SkillTreeHomeworkEntities();
+        }
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
