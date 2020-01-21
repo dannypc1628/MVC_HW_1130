7168c7ca3458050ec7dc2f1d999d2a89325ba959
dannyLiu <h152074716@gmail.com>
2020-01-21 18:17:31 +0800

    移除BundleConfig.cs

--- MVC_HW_191130/Global.asax.cs
+++ MVC_HW_191130/Global.asax.cs
@@ -14,8 +14,7 @@ namespace MVC_HW_191130
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
     }
 }
