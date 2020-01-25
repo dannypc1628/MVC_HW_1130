749aa211b5c0b5a3307bc3adc1a7fc9b615c2bc5
dannyLiu <h152074716@gmail.com>
2020-01-25 17:34:29 +0800

    支出收入標色抽成擴充方法

---
+++ MVC_HW_191130/Helper/BootstrapColorHelper.cs
@@ -0,0 +1,25 @@
+﻿using MVC_HW_191130.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_HW_191130.Helper
+{
+    public static class BootstrapColorHelper
+    {
+        public static string GetPrimaryOrDanger(CategoryEnum category)
+        {
+            var textColor = "";
+            if (category.ToString() == "支出")
+            {
+                textColor = "text-danger";
+            }
+            else
+            {
+                textColor = "text-primary";
+            }
+            return textColor;
+        }
+    }
+}
