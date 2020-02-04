421e77848aa2352678b5de834e8abdf479f167a3
dannyLiu <h152074716@gmail.com>
2020-02-04 11:15:08 +0800

    加入ASP.NET Identity

---
+++ MVC_HW_191130/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_HW_191130.Startup))]
+namespace MVC_HW_191130
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
